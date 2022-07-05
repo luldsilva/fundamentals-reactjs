dc2ad692c7d07abbe9af1f2b4cbe3a4d02a6be1e
Lucas Miha <lucaslisilva@outlook.com>
2022-07-05 16:58:15 -0300

    add sidebar and stylized it

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,20 @@
 import { Header } from "./components/Header";
+import { Post } from "./Post";
+
+import styles from "./App.module.css";
+import "./global.css";
+import { Sidebar } from "./components/Sidebar";
 
 function App() {
   return (
     <div>
       <Header />
-      <h1>Hello World</h1>
+      <div className={styles.wrapper}>
+        <Sidebar/>
+        <main>
+          <Post author="Lucas Silva" content="Conteudo de post para testes" />
+        </main>
+      </div>
     </div>
   );
 }
